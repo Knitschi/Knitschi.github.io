14dc7ff3a7be82e6f32eb5d36f1ab615cf709d02
jenkins <not@valid.com>
2018-10-21 09:16:57 +0000

    Add new CMakeProjectFramework documentation.

---
+++ CMakeProjectFramework/doxygen/navtreedata.js
@@ -0,0 +1,39 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "CMakeProjectFramework", "index.html", [
+    [ "Overview", "index.html", "index" ],
+    [ "Todo List", "dd/da0/todo.html", null ],
+    [ "Bug List", "de/da5/bug.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"d1/d4f/_c_p_f_c_make_test_targets.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
