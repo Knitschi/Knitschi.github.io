78ccfbd5f28d695524824cbf124eb028842dcd63
jenkins <not@valid.com>
2019-01-13 17:16:32 +0000

    Add new CMakeProjectFramework documentation.

---
+++ CMakeProjectFramework/doxygen/html/da/daa/_c_p_f_c_make.js
@@ -0,0 +1,81 @@
+var _c_p_f_c_make =
+[
+    [ "CPFCMake", "da/daa/_c_p_f_c_make.html#CPFCMake", null ],
+    [ "API Documentation", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html", [
+      [ "Global variables and cache variables", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfGlobalVariables", null ],
+      [ "Module cpfInit.cmake", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfInitModule", null ],
+      [ "Module cpfAddPackages.cmake", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddPackagesModule", [
+        [ "cpfAddPackages()", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddPackages", [
+          [ "Arguments", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddPackagesArguments", null ]
+        ] ]
+      ] ],
+      [ "Module cpfInitPackageModule.cmake", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfInitPackageModule", [
+        [ "cpfInitPackageProject()", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfInitPackageProject", null ]
+      ] ],
+      [ "Module cpfAddCppPackage.cmake", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddCppPackageModule", [
+        [ "cpfAddCppPackage()", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddCppPackage", [
+          [ "Arguments", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddCppPackageArguments", null ]
+        ] ]
+      ] ],
+      [ "Module cpfAddFilePackage.cmake", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddFilePackageModule", [
+        [ "cpfAddFilePackage()", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddFilePackage", [
+          [ "Arguments", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddFilePackageArguments", null ]
+        ] ]
+      ] ],
+      [ "Module cpfAddDoxygenPackage.cmake", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddDoxygenPackageModule", [
+        [ "cpfAddDoxygenPackage()", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddDoxygenPackage", [
+          [ "Arguments", "d9/dfc/_c_p_f_c_make_a_p_i_docs.html#cpfAddDoxygenPackageArguments", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "CPF configurations", "d7/d8d/_c_p_f_configuration.html", null ],
+    [ "Custom targets", "df/d03/_c_p_f_custom_targets.html", [
+      [ "The custom targets in a CPF project", "df/d03/_c_p_f_custom_targets.html#CustomTargetsInACPFProject", [
+        [ "Global targets", "df/d03/_c_p_f_custom_targets.html#GlobalTargets", null ],
+        [ "Package targets", "df/d03/_c_p_f_custom_targets.html#PackageTargets", null ],
+        [ "Target annotations", "df/d03/_c_p_f_custom_targets.html#TargetAnnotations", [
+          [ "abi-compliance-checker", "df/d03/_c_p_f_custom_targets.html#abicompliancechecker", null ],
+          [ "acyclic", "df/d03/_c_p_f_custom_targets.html#acyclic", null ],
+          [ "ALL_BUILD", "df/d03/_c_p_f_custom_targets.html#ALL_BUILD", null ],
+          [ "clang-tidy", "df/d03/_c_p_f_custom_targets.html#clang-tidy", null ],
+          [ "distributionPackages", "df/d03/_c_p_f_custom_targets.html#distributionPackages", null ],
+          [ "globalFiles", "df/d03/_c_p_f_custom_targets.html#globalFiles", null ],
+          [ "install", "df/d03/_c_p_f_custom_targets.html#install", null ],
+          [ "opencppcoverage", "df/d03/_c_p_f_custom_targets.html#opencppcoverage", null ],
+          [ "pipeline", "df/d03/_c_p_f_custom_targets.html#pipeline", null ],
+          [ "runAllTests", "df/d03/_c_p_f_custom_targets.html#runAllTests", null ],
+          [ "runFastTests", "df/d03/_c_p_f_custom_targets.html#runFastTests", null ],
+          [ "valgrind", "df/d03/_c_p_f_custom_targets.html#valgrind", null ],
+          [ "ZERO_CHECK", "df/d03/_c_p_f_custom_targets.html#ZERO_CHECK", null ],
+          [ "abi-compliance-checker_<package>", "df/d03/_c_p_f_custom_targets.html#abicompliancechecker_package", null ],
+          [ "clang-tidy_<package>", "df/d03/_c_p_f_custom_targets.html#clangtidy_package", null ],
+          [ "distributionPackages_<package>", "df/d03/_c_p_f_custom_targets.html#distributionPackages_package", null ],
+          [ "opencppcoverage_<package>", "df/d03/_c_p_f_custom_targets.html#opencppcoverage_package", null ],
+          [ "<package>", "df/d03/_c_p_f_custom_targets.html#package", null ],
+          [ "<package>_fixtures", "df/d03/_c_p_f_custom_targets.html#package_fixtures", null ],
+          [ "<package>_tests", "df/d03/_c_p_f_custom_targets.html#package_tests", null ],
+          [ "runAllTests_<package>", "df/d03/_c_p_f_custom_targets.html#runAllTests_package", null ],
+          [ "runFastTests_<package>", "df/d03/_c_p_f_custom_targets.html#runFastTests_package", null ],
+          [ "valgrind_<package>", "df/d03/_c_p_f_custom_targets.html#valgrind_package", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "Test targets", "d1/d4f/_c_p_f_c_make_test_targets.html", null ],
+    [ "Distribution packages", "d3/d64/_c_p_f_distribution_packages.html", null ],
+    [ "Documentation generation", "dc/d68/_c_p_f_documentation_generation.html", [
+      [ "The Doxygen Package", "dc/d68/_c_p_f_documentation_generation.html#CPFDoxygenPackage", [
+        [ "Searching", "dc/d68/_c_p_f_documentation_generation.html#CPFDoxygengSearching", null ],
+        [ "Adding a dependency graph", "dc/d68/_c_p_f_documentation_generation.html#CPFDoxygenDependencyGraph", null ]
+      ] ]
+    ] ],
+    [ "Versioning", "d3/dae/_c_p_f_versioning.html", [
+      [ "Version Tags", "d3/dae/_c_p_f_versioning.html#CPFVersionTags", [
+        [ "Release version format", "d3/dae/_c_p_f_versioning.html#CPFReleaseVersion", null ],
+        [ "Internal version format", "d3/dae/_c_p_f_versioning.html#CPFInternalVersion", null ],
+        [ "Dirty versions", "d3/dae/_c_p_f_versioning.html#CPFDirtyVersions", null ]
+      ] ],
+      [ "Using the version number in the production code", "d3/dae/_c_p_f_versioning.html#CPFUseInProductionCode", null ],
+      [ "Version tags as validation stamps", "d3/dae/_c_p_f_versioning.html#CPFTagsAsValidationStamp", null ],
+      [ "Incrementing versions", "d3/dae/_c_p_f_versioning.html#CPFIncrementingVersions", null ]
+    ] ]
+];
