03e3b97bac8a76900194be00fabb60694fd9d317
jenkins <not@valid.com>
2019-02-21 18:04:48 +0000

    Add new CMakeProjectFramework documentation.

---
+++ CMakeProjectFramework/doxygen/html/da/daa/_c_p_f_c_make.js
@@ -0,0 +1,120 @@
+var _c_p_f_c_make =
+[
+    [ "CPFCMake", "da/daa/_c_p_f_c_make.html#CPFCMake", null ],
+    [ "API Functions", "da/de0/_c_p_f_modules.html", [
+      [ "Argument Notation", "da/de0/_c_p_f_modules.html#cpfArgumentNotation", [
+        [ "Argument Sub-Lists", "da/de0/_c_p_f_modules.html#cpfArgSubLists", null ]
+      ] ],
+      [ "Module cpfInit.cmake", "da/de0/_c_p_f_modules.html#cpfInitModule", null ],
+      [ "Module cpfAddPackages.cmake", "da/de0/_c_p_f_modules.html#cpfAddPackagesModule", [
+        [ "cpfAddPackages()", "da/de0/_c_p_f_modules.html#cpfAddPackages", [
+          [ "Arguments", "da/de0/_c_p_f_modules.html#cpfAddPackagesArguments", null ]
+        ] ]
+      ] ],
+      [ "Module cpfInitPackageModule.cmake", "da/de0/_c_p_f_modules.html#cpfInitPackageModule", [
+        [ "cpfInitPackageProject()", "da/de0/_c_p_f_modules.html#cpfInitPackageProject", null ]
+      ] ],
+      [ "Module cpfAddCppPackage.cmake", "da/de0/_c_p_f_modules.html#cpfAddCppPackageModule", [
+        [ "cpfAddCppPackage()", "da/de0/_c_p_f_modules.html#cpfAddCppPackage", [
+          [ "Arguments", "da/de0/_c_p_f_modules.html#cpfAddCppPackageArguments", [
+            [ "OWNER", "da/de0/_c_p_f_modules.html#cpfArgOwner", null ],
+            [ "WEBPAGE_URL", "da/de0/_c_p_f_modules.html#cpfArgWebpageUrl", null ],
+            [ "MAINTAINER_EMAIL", "da/de0/_c_p_f_modules.html#cpfArgMaintainerEmail", null ],
+            [ "COMPILE_OPTIONS", "da/de0/_c_p_f_modules.html#cpfArgCompileOptions", null ],
+            [ "ENABLE_ABI_API_COMPATIBILITY_REPORT_TARGETS", "da/de0/_c_p_f_modules.html#cpfArgEnableAbiApiCompatibilityReportTargets", null ],
+            [ "ENABLE_ABI_API_STABILITY_CHECK_TARGETS", "da/de0/_c_p_f_modules.html#cpfArgEnableAbiApiStablilityCheckTargets", null ],
+            [ "ENABLE_CLANG_TIDY_TARGET", "da/de0/_c_p_f_modules.html#cpfArgEnableClangTidyTarget", null ],
+            [ "ENABLE_OPENCPPCOVERAGE_TARGET", "da/de0/_c_p_f_modules.html#cpfArgEnableOpenCppCoverageTarget", null ],
+            [ "ENABLE_PACKAGE_DOX_FILE_GENERATION", "da/de0/_c_p_f_modules.html#cpfArgEnablePackageDoxFileGeneration", null ],
+            [ "ENABLE_PRECOMPILED_HEADER", "da/de0/_c_p_f_modules.html#cpfArgEnablePrecompiledHeader", null ],
+            [ "ENABLE_RUN_TESTS_TARGET", "da/de0/_c_p_f_modules.html#cpfArgEnableRunTestsTarget", null ],
+            [ "ENABLE_VALGRIND_TARGET", "da/de0/_c_p_f_modules.html#cpfArgEnableValgrindTarget", null ],
+            [ "ENABLE_VERSION_RC_FILE_GENERATION", "da/de0/_c_p_f_modules.html#cpfArgEnableVersionRCFileGeneration", null ]
+          ] ]
+        ] ],
+        [ "cpfQt5AddUIAndQrcFiles()", "da/de0/_c_p_f_modules.html#cpfQt5AddUIAndQrcFiles", null ]
+      ] ],
+      [ "Module cpfAddFilePackage.cmake", "da/de0/_c_p_f_modules.html#cpfAddFilePackageModule", [
+        [ "cpfAddFilePackage()", "da/de0/_c_p_f_modules.html#cpfAddFilePackage", [
+          [ "Arguments", "da/de0/_c_p_f_modules.html#cpfAddFilePackageArguments", null ]
+        ] ]
+      ] ],
+      [ "Module cpfAddDoxygenPackage.cmake", "da/de0/_c_p_f_modules.html#cpfAddDoxygenPackageModule", [
+        [ "cpfAddDoxygenPackage()", "da/de0/_c_p_f_modules.html#cpfAddDoxygenPackage", [
+          [ "Arguments", "da/de0/_c_p_f_modules.html#cpfAddDoxygenPackageArguments", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "API Global Variables", "d4/d91/_c_p_f_global_variables.html", [
+      [ "Global variables and cache variables", "d4/d91/_c_p_f_global_variables.html#cpfGlobalVariables", [
+        [ "CPF Variables", "d4/d91/_c_p_f_global_variables.html#cpfVariables", [
+          [ "CPF_CI_PROJECT", "d4/d91/_c_p_f_global_variables.html#CPF_CI_PROJECT", null ],
+          [ "CPF_WEBSERVER_BASE_DIR", "d4/d91/_c_p_f_global_variables.html#CPF_WEBSERVER_BASE_DIR", null ],
+          [ "CPF_MINIMUM_CMAKE_VERSION", "d4/d91/_c_p_f_global_variables.html#CPF_MINIMUM_CMAKE_VERSION", null ],
+          [ "CPF_VERBOSE", "d4/d91/_c_p_f_global_variables.html#CPF_VERBOSE", null ],
+          [ "CPF_ENABLE_ACYCLIC_TARGET", "d4/d91/_c_p_f_global_variables.html#CPF_ENABLE_ACYCLIC_TARGET", null ]
+        ] ],
+        [ "Global defaults for the cpfAddCppPackage() function.", "d4/d91/_c_p_f_global_variables.html#cpfVariable", null ],
+        [ "Allowed CMake Variables", "d4/d91/_c_p_f_global_variables.html#cpfAllowedCMakeVariables", null ],
+        [ "Overridden CMake Variables", "d4/d91/_c_p_f_global_variables.html#cpfOverriddenCMakeVariables", null ],
+        [ "Overridden target properties", "d4/d91/_c_p_f_global_variables.html#cpfOverriddenTargetProperties", null ]
+      ] ]
+    ] ],
+    [ "CPF configurations", "d7/d8d/_c_p_f_configuration.html", null ],
+    [ "Custom targets", "df/d03/_c_p_f_custom_targets.html", [
+      [ "The custom targets in a CPF project", "df/d03/_c_p_f_custom_targets.html#CustomTargetsInACPFProject", [
+        [ "Global targets", "df/d03/_c_p_f_custom_targets.html#GlobalTargets", null ],
+        [ "Package targets", "df/d03/_c_p_f_custom_targets.html#PackageTargets", null ],
+        [ "Private targets", "df/d03/_c_p_f_custom_targets.html#PrivateTargets", null ],
+        [ "Target annotations", "df/d03/_c_p_f_custom_targets.html#TargetAnnotations", [
+          [ "abi-compliance-checker", "df/d03/_c_p_f_custom_targets.html#abicompliancechecker", null ],
+          [ "acyclic", "df/d03/_c_p_f_custom_targets.html#acyclic", null ],
+          [ "ALL_BUILD", "df/d03/_c_p_f_custom_targets.html#ALL_BUILD", null ],
+          [ "clang-tidy", "df/d03/_c_p_f_custom_targets.html#clang-tidy", null ],
+          [ "distributionPackages", "df/d03/_c_p_f_custom_targets.html#distributionPackages", null ],
+          [ "globalFiles", "df/d03/_c_p_f_custom_targets.html#globalFiles", null ],
+          [ "install", "df/d03/_c_p_f_custom_targets.html#install", null ],
+          [ "opencppcoverage", "df/d03/_c_p_f_custom_targets.html#opencppcoverage", null ],
+          [ "pipeline", "df/d03/_c_p_f_custom_targets.html#pipeline", null ],
+          [ "runAllTests", "df/d03/_c_p_f_custom_targets.html#runAllTests", null ],
+          [ "runFastTests", "df/d03/_c_p_f_custom_targets.html#runFastTests", null ],
+          [ "valgrind", "df/d03/_c_p_f_custom_targets.html#valgrind", null ],
+          [ "ZERO_CHECK", "df/d03/_c_p_f_custom_targets.html#ZERO_CHECK", null ],
+          [ "abi-compliance-checker_<package>", "df/d03/_c_p_f_custom_targets.html#abicompliancechecker_package", null ],
+          [ "clang-tidy_<package>", "df/d03/_c_p_f_custom_targets.html#clangtidy_package", null ],
+          [ "distributionPackages_<package>", "df/d03/_c_p_f_custom_targets.html#distributionPackages_package", null ],
+          [ "opencppcoverage_<package>", "df/d03/_c_p_f_custom_targets.html#opencppcoverage_package", null ],
+          [ "<package>", "df/d03/_c_p_f_custom_targets.html#package", null ],
+          [ "<package>_fixtures", "df/d03/_c_p_f_custom_targets.html#package_fixtures", null ],
+          [ "<package>_tests", "df/d03/_c_p_f_custom_targets.html#package_tests", null ],
+          [ "runAllTests_<package>", "df/d03/_c_p_f_custom_targets.html#runAllTests_package", null ],
+          [ "runFastTests_<package>", "df/d03/_c_p_f_custom_targets.html#runFastTests_package", null ],
+          [ "valgrind_<package>", "df/d03/_c_p_f_custom_targets.html#valgrind_package", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "The packages.cmake file", "dd/dc8/_c_p_f_packages_c_make_file.html", null ],
+    [ "Test targets", "d1/d4f/_c_p_f_c_make_test_targets.html", null ],
+    [ "Distribution packages", "d3/d64/_c_p_f_distribution_packages.html", null ],
+    [ "Documentation generation", "dc/d68/_c_p_f_documentation_generation.html", [
+      [ "The Doxygen Package", "dc/d68/_c_p_f_documentation_generation.html#CPFDoxygenPackage", [
+        [ "Searching", "dc/d68/_c_p_f_documentation_generation.html#CPFDoxygengSearching", null ],
+        [ "Adding a dependency graph", "dc/d68/_c_p_f_documentation_generation.html#CPFDoxygenDependencyGraph", null ]
+      ] ]
+    ] ],
+    [ "Versioning", "d3/dae/_c_p_f_versioning.html", [
+      [ "Version Tags", "d3/dae/_c_p_f_versioning.html#CPFVersionTags", [
+        [ "Release version format", "d3/dae/_c_p_f_versioning.html#CPFReleaseVersion", null ],
+        [ "Internal version format", "d3/dae/_c_p_f_versioning.html#CPFInternalVersion", null ],
+        [ "Dirty versions", "d3/dae/_c_p_f_versioning.html#CPFDirtyVersions", null ]
+      ] ],
+      [ "Using the version number in the production code", "d3/dae/_c_p_f_versioning.html#CPFUseInProductionCode", null ],
+      [ "Version tags as validation stamps", "d3/dae/_c_p_f_versioning.html#CPFTagsAsValidationStamp", null ],
+      [ "Incrementing versions", "d3/dae/_c_p_f_versioning.html#CPFIncrementingVersions", null ]
+    ] ],
+    [ "Known Problems", "d9/d89/_c_p_f_c_make_known_problems.html", [
+      [ "Known Problems and Workarounds", "d9/d89/_c_p_f_c_make_known_problems.html#CPFCMakeKnownProblemsSection", [
+        [ "Problem when using Qt CMAKE_AUTOUIC and CMAKE_AUTORCC in combination with CPF_ENABLE_PRECOMPILED_HEADER", "d9/d89/_c_p_f_c_make_known_problems.html#CotireQtIncompatibility", null ]
+      ] ]
+    ] ]
+];
