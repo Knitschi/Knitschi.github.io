c45493f8505fbe18a9a6bf548a707ec783041958
jenkins <not@valid.com>
2019-03-24 18:53:19 +0000

    Add new CMakeProjectFramework documentation.

---
+++ CMakeProjectFramework/doxygen/html/df/de6/_c_p_f_buildscripts.js
@@ -0,0 +1,14 @@
+var _c_p_f_buildscripts =
+[
+    [ "CPFBuildscripts", "df/de6/_c_p_f_buildscripts.html#CPFBuildscripts", null ],
+    [ "0_CopyScripts.py", "d6/de5/script_copy_files.html", null ],
+    [ "1_Configure.py", "da/dbe/script_configure.html", [
+      [ "Command Line Interface", "da/dbe/script_configure.html#CLI1_Configure", null ]
+    ] ],
+    [ "2_Generate.py", "d4/da9/script_generate.html", [
+      [ "Command Line Interface", "d4/da9/script_generate.html#CLI2_Generate", null ]
+    ] ],
+    [ "3_Make.py", "d4/d60/script_make.html", [
+      [ "Command Line Interface", "d4/d60/script_make.html#CLI3_Make", null ]
+    ] ]
+];
