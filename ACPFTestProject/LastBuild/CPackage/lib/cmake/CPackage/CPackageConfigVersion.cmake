8ed32daadb2755858ee8f5d5ec505521b3a99ae7
jenkins <not@valid.com>
2019-08-11 16:29:06 +0000

    Updates "ACPFTestProject" with the build results of version 1.1.6.24-5975

--- ACPFTestProject/LastBuild/CPackage/lib/cmake/CPackage/CPackageConfigVersion.cmake
+++ ACPFTestProject/LastBuild/CPackage/lib/cmake/CPackage/CPackageConfigVersion.cmake
@@ -9,12 +9,12 @@
 # The variable CVF_VERSION must be set before calling configure_file().
 
 
-set(PACKAGE_VERSION "1.1.6.22-abdc")
+set(PACKAGE_VERSION "1.1.6.24-5975")
 
-if("1.1.6.22-abdc" MATCHES "^([0-9]+\\.[0-9]+\\.[0-9]+)\\.") # strip the tweak version
+if("1.1.6.24-5975" MATCHES "^([0-9]+\\.[0-9]+\\.[0-9]+)\\.") # strip the tweak version
   set(CVF_VERSION_NO_TWEAK "${CMAKE_MATCH_1}")
 else()
-  set(CVF_VERSION_NO_TWEAK "1.1.6.22-abdc")
+  set(CVF_VERSION_NO_TWEAK "1.1.6.24-5975")
 endif()
 
 if(PACKAGE_FIND_VERSION MATCHES "^([0-9]+\\.[0-9]+\\.[0-9]+)\\.") # strip the tweak version
