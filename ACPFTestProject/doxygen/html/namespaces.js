0c489961e2526963ff807ea51f41d45b54939ef2
Knitschi <knitschi@freenet.de>
2019-08-11 19:04:58 +0200

    Cleans up the old directory structure.

--- ACPFTestProject/doxygen/html/namespaces.js
+++
@@ -1,7 +0,0 @@
-var namespaces =
-[
-    [ "a", "d2/dcf/namespacea.html", null ],
-    [ "c", "de/d43/namespacec.html", null ],
-    [ "d", "df/d9e/namespaced.html", null ],
-    [ "e", "d6/de2/namespacee.html", null ]
-];
