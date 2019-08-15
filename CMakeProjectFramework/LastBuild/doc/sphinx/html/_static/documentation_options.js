7e6e9ee25fae0dc1585e1b33b4361343a81d771e
jenkins <not@valid.com>
2019-08-15 19:24:44 +0000

    Updates "CMakeProjectFramework" with the build results of version 0.0.0.508-c08b

--- CMakeProjectFramework/LastBuild/doc/sphinx/html/_static/documentation_options.js
+++ CMakeProjectFramework/LastBuild/doc/sphinx/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0.507-e2d9',
+    VERSION: '0.0.0.508-c08b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
